23bcf885d3df09ba2fd87bcf6b45094650b24552
unknown <21PC08@cs2k16.amcs.ws2k12.psgcs.edu>
2024-01-06 16:32:44 +0530

    Commit

--- level1a.py
+++ level1a.py
@@ -1,13 +1,13 @@
 import json
 
-with open(r'Z:\Hackathon\Input data\level1.json') as file:
+with open(r'Z:\Hackathon\Input data\level1a.json') as file:
     data = json.load(file)
 
-neighbourhoods=data['neighbourhoods']
-restaurant_distances=data['restaurants']['r0']['neighbourhood_distances']
-vehicle_capacity=data['vehicles']['v0']['capacity']
+neighborhoods = data['neighbourhoods']
+restaurant_distances = data['restaurants']['r0']['neighbourhood_distance']
+vehicle_capacity = data['vehicles']['v0']['capacity']
 
-n_neighbourhoods=len(neighbourhoods)
+n_neighbourhoods = len(neighborhoods)
 orders = [(f"n{i}", neighborhood['order_quantity'], neighborhood['distances']) for i, neighborhood in neighborhoods.items()]
 orders.sort(key=lambda x: sum(x[2]))
 
@@ -27,11 +27,15 @@ def allocate_delivery_slots(orders, vehicle_capacity):
 
     if current_slot['neighborhoods']:
         slots.append(current_slot)
-
     return slots
 
 delivery_slots = allocate_delivery_slots(orders, vehicle_capacity)
+output = {"v0": {}}
 
-output={"v0":{}}
-for idx,slot in enumerate(delivery_slots):
-    output["v0"][f"path{idx+1}"]
+for idx, slot in enumerate(delivery_slots):
+    output["v0"][f"path{idx+1}"] = [neighborhood.replace("nn", "n") for neighborhood in ["r0"] + slot['neighborhoods'] + ["r0"]]
+
+output_string = json.dumps(output, separators=(',', ':'))
+with open('outfile1a.json', 'w') as json_file:
+    json_file.write(output_string)
+print(output)
